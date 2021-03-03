8c9c05786e763d1df20966d6102463795d75a17a
anderleand <andersonleandro.desouza@gmail.com>
2021-03-03 17:36:40 -0300

    Trabalhando com formulários e templates

--- src/app/course/course-info.component.ts
+++ src/app/course/course-info.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     templateUrl:'./course-info.component.html'
 })
 export class CourseInfoComponent implements OnInit {
     
-    courseId: number;
+    course: Course;
 
-    constructor(private activateRoute: ActivatedRoute){
+    constructor(private activateRoute: ActivatedRoute, private CourseService: CourseService){
 
     }
 
     ngOnInit(): void{
-        this.courseId = +this.activateRoute.snapshot.paramMap.get('id');
+        this.course = this.CourseService.retrieveById(+this.activateRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(): void {
+        this.CourseService.save(this.course);
     }
 
 }
